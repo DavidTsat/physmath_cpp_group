a94330b0ed308b4522736dd5f388d4edb2d3ef55
David Tsaturyan <davidt@tce.loc>
2022-06-16 12:28:10 +0400

    added homework on regexp

--- lesson 13 - regexp/Source.cpp
+++ lesson 13 - regexp/Source.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <iterator>
 #include "csv_parse.h"
-#include "split.h"
 
 using namespace std;
 
@@ -304,66 +303,6 @@ Valid email address matching
 	[-+]?[0-9]*\.?[0-9]*
 	[-+]?([0-9]*\.[0-9]+|[0-9]+)
 	[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?
-*/
-
-/*
-Excercises
-0. match lines with even number lengths
-^(?=(..))\1+$
-a
-aa
-aaa
-aaaa
-aaaaa
-aaaaaa
-aaaaaaa
-aaaaaaaa
-aaaaaaaaa
-aaaaaaaaaa
-aaaaaaaaaaa
-...
-
-0. match first half only
-(?=^(a+)\1)\1
-a
-aa
-aaa
-aaaa
-aaaaa
-aaaaaa
-aaaaaaa
-aaaaaaaa
-aaaaaaaaa
-aaaaaaaaaa
-aaaaaaaaaaa
-
-1. https://regex.sketchengine.co.uk/cgi/ex1.cgi
-\b.*(pi)?(?(1)|s).*\b
-
-2. https://regex.sketchengine.co.uk/cgi/ex2.cgi
-ap(?!(e |[pd]))
-
-3. https://alf.nu/RegexGolf?world=regex&level=r02
-^[a-f]{4}
-
-4. https://alf.nu/RegexGolf?world=regex&level=r03
-(\w{3,})+\w*(\1).*
-
-5. https://alf.nu/RegexGolf?world=regex&level=r04
-
-^(?!(\w)+\1)\w*$|ef or ^(?!.*(\w)(\w)\2\1).*$
-
-6. https://alf.nu/RegexGolf?world=regex&level=r05
-^(\w)(\w)\w*(\2)(\1)$ or (?=(\w)(\w)(\w)?(\2)(\1))    (?=(\w)(\w)[^r]?(\2)(\1))
-
-7. https://alf.nu/RegexGolf?world=regex&level=r06
-^(?!(..+)\1+$) or ^(?!(x{2,})\1+$)    (?!^(x{2,})\1+$)
-
-8. https://alf.nu/RegexGolf?world=regex&level=r07
-(?=(.).\1.\1.\1) or (.).\1.\1.\1.*
-
-9. https://alf.nu/RegexGolf?world=regex&level=r08
-^[^o]\w{4,5}$
 */
 
 void reg_match(const regex& re, const string& s)
